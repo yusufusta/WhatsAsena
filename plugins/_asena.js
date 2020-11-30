4b4aab8f9167dbb3e363e9e10c0d1b13d9044986
abduljaleel986 <75239973+abduljaleel986@users.noreply.github.com>
2020-11-30 12:50:18 +0530

    Update _asena.js

--- plugins/_asena.js
+++ plugins/_asena.js
@@ -13,7 +13,7 @@ const Language = require('../language');
 
 const Lang = Language.getString('_asena');
 
-Asena.addCommand({pattern: 'asena', fromMe: true}, (async (message, match) => {
+Asena.addCommand({pattern: 'asena', fromall: true}, (async (message, match) => {
     var CMD_HELP = '';
 
     Asena.commands.map(
