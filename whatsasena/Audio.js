2bde4ceede3cb70ca72d5803287b4f376504c74b
lyfe00011 <76509367+lyfe00011@users.noreply.github.com>
2021-01-05 12:45:27 +0530

    Create Audio.js

---
+++ whatsasena/Audio.js
@@ -0,0 +1,113 @@
+/* Copyright (C) 2020 Yusuf Usta.
+
+Licensed under the  GPL-3.0 License;
+
+you may not use this file except in compliance with the License.
+
+WhatsAsena - Yusuf Usta
+
+*/
+
+const {MessageType, Presence, MessageOptions} = require('@adiwajshing/baileys');
+
+const Base = require('./Base');
+
+const Message = require('./Message');
+
+const ReplyMessage = require('./ReplyMessage');
+
+class Audio extends Base {
+
+    constructor(client, data) {
+
+        super(client);
+
+        if (data) this._patch(data);
+
+    }
+
+    _patch(data) {
+
+        this.id = data.key.id === undefined ? undefined : data.key.id;
+
+        this.jid = data.key.remoteJid;
+
+        this.fromMe = data.key.fromMe;
+
+        this.caption = data.message.audioMessage.caption === null ? data.message.audioMessage.caption : '';
+
+        this.url = data.message.audioMessage.url;
+
+        this.timestamp = typeof(data.messageTimestamp) === 'object' ? data.messageTimestamp.low : data.messageTimestamp;
+
+        this.mimetype = data.message.audioMessage.mimetype;
+
+        this.height = data.message.videoMessage.height;
+
+        this.width = data.message.videoMessage.width;
+
+        this.mediaKey = data.message.audioMessage.mediaKey;
+
+        this.data = data;
+
+        
+
+        if (data.message.audioMessage.hasOwnProperty('contextInfo') && data.message.contextInfo.quotedMessage) { 
+
+            this.reply_message = new ReplyMessage(this.client, data.message.audioMessage.contextInfo); }
+
+        else {
+
+            this.reply_message = false;
+
+        }
+
+        
+
+        return super._patch(data);
+
+    }
+
+    async delete() {
+
+        return await this.client.deleteMessage(this.jid, {id: this.id, remoteJid: this.jid, fromMe: true})
+
+    }
+
+    async reply(text) {
+
+        var message = await this.client.sendMessage(this.jid, text, MessageType.text, {quoted: this.data})
+
+        return new Message(this.client, message)
+
+    }
+
+    async sendMessage(content, type, options) {
+
+        return await this.client.sendMessage(this.jid, content, type, options)
+
+    }
+
+    async sendTyping() {
+
+        return await this.client.updatePresence(this.jid, Presence.composing) ;
+
+    }
+
+    async sendRead() {
+
+        return await this.client.chatRead(this.jid);
+
+    }
+
+    async download(location = this.id) {
+
+        await this.client.downloadAndSaveMediaMessage(this.data, location);
+
+        return this.id + '.' + this.mimetype.split('/')[1];
+
+    }
+
+};
+
+module.exports = Audio;
